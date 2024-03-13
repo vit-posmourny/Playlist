304892647b59691404265a8d82ca97963d5e2969
vit-posmourny <dr1v3r@email.cz>
2024-03-13 17:47:31 +0100

    ToDo: left nav.disclosure JS reakce na klik na Summary (funguje) v1

--- Views/partials/disclosure_nav_genre.php
+++ Views/partials/disclosure_nav_genre.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'genres.php';
-
+// 'genres' je název klíče z pole přiřazeného do prom. $disclosure_nav v Core\View (původně také název klíče, ještě v MainController.php)
 $string = $disclosure_nav['genres'];
 $delimiter = ":";
 // Split the string into an array using the delimiter
@@ -15,10 +15,6 @@ foreach ($explodeArr as $genre)
         <summary><h2>'.$genres[$genre].'</h2></summary>
         <ul>
             <li>1</li>
-            <li> 2</li>
-            <li> 3</li>
-            <li> 4</li>
-            <li> 5</li>
         </ul>
     </details>
     ';
