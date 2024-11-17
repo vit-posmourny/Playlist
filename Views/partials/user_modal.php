daa7f11bb89f2dcd482c0b70d2bf61c83cbb6b28
vit-posmourny <dr1v3r@email.cz>
2024-11-17 01:32:20 +0100

    drobna zmena nazvu

--- Views/partials/user_modal.php
+++ Views/partials/user_modal.php
@@ -1,16 +1,12 @@
 <div id="d-usracc-modal" class="usracc_modal" popover="auto">
 
-    <button id="b-user-foto" popovertarget="d-add-pict-modal">
+    <button id="b-user-foto" popovertarget="d-crop-modal">
         <img id="i-user-foto" alt="user foto">
     </button>
-
-    <div class="text_area">
-        <p id="mouseCoords"></p>
-    </div>
 
 </div>
 
 <div id="d-crop-modal" class="crop_modal" popover="auto"></div>
 
 <script type="text/javascript"> <?php include "..\js\usracc_modal.js"?> </script>
-<script type="text/javascript"> <?php include "..\js\user_foto_modal.js"?> </script>
+<script type="text/javascript"> <?php include "..\js\croppie_modal.js"?> </script>
