8682991ccce7899503244c0037c5cd2179e1a5c8
vit-posmourny <dr1v3r@email.cz>
2025-07-29 14:29:53 +0200

    přidal obr. pro readme

--- Views/js/croppie_modal.js
+++
@@ -1,8 +0,0 @@
-var imgBtn = document.getElementById('i-user-foto');
-
-imgBtn.addEventListener('click', openDialogInNewWindow);
-
-function openDialogInNewWindow() {
-    // Define the content for the new window
-    const dialogWindow = window.open("/Playlist/croppie", "DialogWindow", "width=505,height=900,menubar=no,resizable=no,status=no,titlebar=no,left=300");
-}
